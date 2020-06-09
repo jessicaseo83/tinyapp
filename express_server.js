3cc9e0a98de4e597bfad3892ba608aba2739151e
Jessica Seo <jessicaseo83@gmail.com>
2020-06-09 23:42:57 +0000

    Updating URLs

--- express_server.js
+++ express_server.js
@@ -3,18 +3,20 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
 
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 }
 
 const urlDatabase = {
+  //shortURL:  longURL
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view engine", "ejs");
-
+// trigger a listen action on a specific port and do a callback
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
@@ -43,8 +45,8 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[shortURL];
   
   res.redirect(longURL);
-})
-
+});
+// In the event of a get 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -55,16 +57,27 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[shortURL];
   
   res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+
+  res.redirect("/urls");
 })
+
+
+
 
 
 app.use((err,req, res, next) => {
   console.log(err.message);
   res.status(404).send("Something went wrong!")
-})
+});
 
 
-
+//if we have a get request asking for the path of '/', do the callback
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
